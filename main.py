5a031848f8ac950e81c4b9eccc16fe7efa43599a
YOUMBI Harold <80352823+Youmbi-Harold@users.noreply.github.com>
2022-04-08 09:07:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Review: 
+# Create a function called greet().
+def greet(name):
+  print(f"Hello, Good Morning {name}")
+  print(f"Please {name}, could  you show me the way out?")
+  print(f"Thanks {name}, Have a Good day!!")
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+greet("Laura")
+
+
